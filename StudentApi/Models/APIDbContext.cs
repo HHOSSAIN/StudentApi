c81759eb7e08cafaaeac73fbb4d2060e79c80603
Hasne Hossain <hasne.hossain1999@gmail.com>
2023-07-16 15:17:14 +1000

    EF core setup with sql server + create db with PM console

---
+++ StudentApi/Models/APIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentApi.Models
+{
+    //inherit the dbContext class from EF core to i)do establish a db connection,
+    //ii)act as middleware between model and db table(mapping)
+    public class APIDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        //base(option) call is used to invoke the constructor of the base class (parent class, i.e. dbContext)
+        //from within the constructor of the derived class (child class, i.e. APIDbContext)
+        //By calling base(option), you ensure that the base class's constructor is executed before the derived class's
+        //constructor, enabling proper initialization of the base class's state.
+        //with the arg "DbConextOptions", we will specify the db provider we'll use & connection string to db...we can do this in program.cs
+        public APIDbContext(DbContextOptions option) : base(option)
+        { }
+    }
+}
